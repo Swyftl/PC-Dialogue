f3c98eb3eceafc438eb433d6731ec8d81b009e51
Swyftl <juniorjonswift@gmail.com>
2025-06-17 00:53:16 +0100

    Starting work on a custom right click context addon

---
+++ addons/phantomcloud_dialogue/ContextMenu.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class ContextMenu : EditorContextMenuPlugin
+{
+    public override void _PopupMenu(string[] paths)
+    {
+    }
+
+    public void _create_new_dialogue()
+    {
+    }
+}
